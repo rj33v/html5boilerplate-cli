8a222120fee48157ee0c74978ceb0fc13a6867cd
rj333v <>
2020-08-29 08:16:16 +0100

    pass in parameters for customisation

--- index.js
+++ index.js
@@ -1,21 +1,27 @@
 #!/usr/bin/env node
 
-
 const fs = require('fs');
+
+const args = process.argv.slice(2);
+let fileName = args[0] ? `${args[0]}.html` : 'index.html';
+let title = args[1] || 'Title';
 
 const html = `<!DOCTYPE html>
 <html>
     <head>
         <meta charset="utf-8">
-        <title>Title>
+        <title>${title}</title>
     </head>
     <body>
     </body>
 </html>`;
 
-fs.writeFile('index.html', html, error => {
+fs.writeFile(fileName, html, error => {
     if (error) {
         console.log(error)
     }
 });
 console.log( '\u279c' + " Boilerplate created" );
+
+
+console.log(args);
